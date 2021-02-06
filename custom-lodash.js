75ab15b6c0a89b356f4d464f09758b0d58bf4f45
Arkadii <veretilin1@gmail.com>
2021-02-06 19:35:05 +0200

    (New method) chunk

--- custom-lodash.js
+++ custom-lodash.js
@@ -1,3 +1,24 @@
-export class Lodash {}
+export class Lodash {
+  chunk(array = [], size = 1) {
+    const outArray = [];
+    if (
+      !array.length ||
+      !Array.isArray(array) ||
+      size < 1 ||
+      typeof size !== 'number'
+    ) {
+      return outArray;
+    }
+    let tempArray = [];
+    for (let i in array) {
+      tempArray.push(array[i]);
+      if (tempArray.length === size || +i === array.length - 1) {
+        outArray.push(tempArray);
+        tempArray = [];
+      }
+    }
+    return outArray;
+  }
+}
 
 export default Lodash;
